983b78dd47e00f56b0cc46938f703dbe430b6214
senthilva <senthil.vinayagam@gmail.com>
2021-06-30 20:42:00 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -14,13 +14,18 @@ import torch.nn as nn
 import torch.nn.init as init
 import torchvision.transforms as transforms
 
-def train_transform():
+def transform_train():
     return transforms.Compose([
                       transforms.RandomCrop(32, padding=4),
                       transforms.RandomHorizontalFlip(),
                       transforms.ToTensor(),
                       transforms.Normalize((0.4914, 0.4822, 0.4465), 
                                            (0.2023, 0.1994, 0.2010)),])
+def transform_test():
+    return transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize((0.4914, 0.4822, 0.4465), 
+                                     (0.2023, 0.1994, 0.2010)),])
 
 def get_mean_and_std(dataset):
     '''Compute the mean and std value of dataset.'''
