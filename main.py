b586d97196fd3eef68e78a65bb52a851b49230ea
senthilva <senthil.vinayagam@gmail.com>
2021-06-30 21:45:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -36,12 +36,12 @@ print('==> Preparing data..')
 trainset = torchvision.datasets.CIFAR10(
     root='./data', train=True, download=True, transform=transform_train())
 trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=128, shuffle=True, num_workers=2)
+    trainset, batch_size=batch_size, shuffle=True, num_workers=2)
 
 testset = torchvision.datasets.CIFAR10(
     root='./data', train=False, download=True, transform=transform_test())
 testloader = torch.utils.data.DataLoader(
-    testset, batch_size=100, shuffle=False, num_workers=2)
+    testset, batch_size=batch_size, shuffle=False, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer',
            'dog', 'frog', 'horse', 'ship', 'truck')
