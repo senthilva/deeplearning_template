275525be2c4e23430eb1f063c2c0823f3084594c
senthilva <senthil.vinayagam@gmail.com>
2021-07-07 22:04:16 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -33,7 +33,8 @@ def transform_train():
 def transform_test():
     return transforms.Compose([
                 transforms.ToTensor(),
-                transforms.Normalize((0.4914, 0.4822, 0.4465), ])
+                transforms.Normalize((0.4914, 0.4822, 0.4465), 
+                                     (0.2023, 0.1994, 0.2010), ])
 
 def transform_trainv2():
     return A.Compose(
