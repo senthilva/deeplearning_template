070eaaaf8c3879f47f295b6b98e2d6253734a1d4
senthilva <senthil.vinayagam@gmail.com>
2021-07-07 22:50:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ args = parser.parse_args()
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 best_acc = 0  # best test accuracy
-start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+start_epoch = 1  # start from epoch 0 or last checkpoint epoch
 
 # Data
 print('==> Preparing data..')
