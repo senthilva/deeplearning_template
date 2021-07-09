396f0f7af937d2b08133f125591a07d039698d6e
senthilva <senthil.vinayagam@gmail.com>
2021-07-09 19:50:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -104,7 +104,7 @@ if args.resume:
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=args.lr,
-                      momentum=0.9)
+                      momentum=0.9,weight_decay=5e-4)
 scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=0.4,
                                                 pct_start = 0.5,
                                                 total_steps = 3000)
